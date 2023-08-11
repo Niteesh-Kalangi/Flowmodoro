4f1d3a3766af07117bb7adedbffec86ce3568d07
Niteesh-Kalangi <niteesh.s.kalangi@gmail.com>
2023-08-11 13:11:58 -0400

    gffgg

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from "cors";
+import { config } from "dotenv";
+config()
+import articles from "./routes/articles.js";
+
+const PORT = 8000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//app.use("/", articles);
+app.get("/backend/hello/", (req, res) => {
+  res.json({
+      message: "Hello World"
+  });
+});
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
