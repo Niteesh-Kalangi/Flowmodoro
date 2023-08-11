e5712dab6337701bc4bfc572eeee96890bd874a9
Niteesh-Kalangi <niteesh.s.kalangi@gmail.com>
2023-08-11 12:43:01 -0400

    changed to index.js

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from "cors";
+import { config } from "dotenv";
+config()
+import articles from "./routes/articles.js";
+
+const PORT = 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/articles", articles);
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
