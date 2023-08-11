2de61a2ec809d1ab10fc3e205d4c38dd442e2f8c
Niteesh-Kalangi <niteesh.s.kalangi@gmail.com>
2023-08-11 14:26:25 -0400

    fdf

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin: ["https://flowmoro-frontend-asz76ya6k-niteesh-kalangi.vercel.app"],
+    origin: ["https://flowmoro-frontend.vercel.app"],
     methods:  ["GET", "POST"],
     credentials: true
 
