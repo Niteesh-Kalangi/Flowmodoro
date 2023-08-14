cbd4f8ba3d36908eab07f9dbf5c168bc34727582
dhanushk03 <35636765+dhanushk03@users.noreply.github.com>
2023-08-13 22:45:42 -0400

    Update Navbar.js

--- timerapp/src/Components/Navbar.js
+++ timerapp/src/Components/Navbar.js
@@ -5,7 +5,7 @@ import logo from "./flowmoro_logo.png";
 const Navbar = (props) => {
     return (
         <div className="navbar">
-                <NavLink to="/" className={"navbar-item navbar-left"} activeClassName="active" activeStyle={{ color: "orange" }} id="homepage"> <li className="flmdlogo"><img src={logo} alt="logo" width="50px" height="50px"/></li>&#160;&#160;&#160;Flowmoro</NavLink>
+                <NavLink to="/" className={"navbar-item navbar-left"} activeClassName="active" activeStyle={{ color: "orange" }} id="homepage"> <li className="flmdlogo"><img src={logo} alt="logo" width="50px" height="50px"/></li>&#160;&#160;&#160;<b>Flowmoro</b></NavLink>
                 <NavLink to="/todolist" className={"navbar-item navbar-right"} activeClassName="active" activeStyle={{ color: "orange" }} id="todolistnav"> To-do List </NavLink>
                 <NavLink to="/timer" className={"navbar-item navbar-right"} activeClassName="active" activeStyle={{ color: "orange" }} id="timer"> Timer </NavLink>
                 <NavLink to="/focuslog" className={"navbar-item navbar-right"} activeClassName="active" activeStyle={{ color: "orange" }} id="studylognav"> Focus Log </NavLink>
