510efd67f526623f968a4c2c6076761086e3f5cb
dhanushk03 <35636765+dhanushk03@users.noreply.github.com>
2023-08-13 22:44:03 -0400

    Update TimerTodoListItem.js

--- timerapp/src/Components/CountdownTimer/TimerTodoListItem.js
+++ timerapp/src/Components/CountdownTimer/TimerTodoListItem.js
@@ -1,48 +1,44 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./TimerTodoListItem.css";
 
 const TimerTodoListItem = (props) => {
-    const [inProgress, setInProgress]= useState(() => {
-        return props.isActive(props.id, props.title);
-    });
+    // const [inProgress, setInProgress]= useState(() => {
+    //     return props.isActive(props.id);
+    // });
 
-    var bg = "transparent";
-    var textcolor = "white";
+    // useEffect(() => {
+    //     const intervalId = setInterval(() => {
+    //         setInProgress(props.isActive(props.id, props.title));
+    //     }, 1000);
+    //     return () => clearTimeout(intervalId);
+    // }, []);
 
-    function initializeBackground() {
-        if (inProgress) {
-            bg = "#5c02c9";
-            textcolor = "orange";
-        } else {
-            bg = "#7b00e8";
-            textcolor = "white";
-        }
-    }
+    // var bg = "transparent";
+    // var textcolor = "white";
 
-    initializeBackground();
+    // function initializeBackground() {
+    //     if (inProgress) {
+    //         bg = "#5c02c9";
+    //         textcolor = "orange";
+    //     } else {
+    //         bg = "#7b00e8";
+    //         textcolor = "white";
+    //     }
+    // }
+
+    // initializeBackground();
 
     return (
-        <div className="timertodolistitem" id={`timertodolistitem${props.id}`} style={{"background":bg, "color":textcolor}} onClick={(e) => {
-            var item = document.getElementById(`timertodolistitem${props.id}`);
-            if (inProgress) {
-                item.style.background = "#7b00e8";
-                item.style.color = "white";
-                setInProgress(false);
-            } else {
-                item.style.background = "#5c02c9";
-                item.style.color = "orange";
-                setInProgress(true);
-            }
-            props.updateActive(props.id, props.title);
-            e.stopPropagation();
-        }}>
-            {/* <button onClick={props.completeTask(props.id)} className="timertodolistitemcompletebutton" style={{"float": "right", "height": "10px", "width": "10px"}}>
-                &#10003;
-            </button> */}
-            <div className="timertodolistitemcompletebutton" onClick={(e) => {props.completeTask(props.id, e); props.incrementCompleted((prev) => {return prev + 1;});}}>&#10003;</div>
-            {/* <p style={{"float": "right"}}>Sample text</p> */}
-            <h4 className="timertodotitle">{props.title}</h4>
+        <div style={{"width": "100%", "display": "flex", "marginRight": "auto"}}>
+            <div className="timertodolistitemcompletebutton" style={{"marginLeft": "auto", "marginRight": "auto"}} onClick={(e) => {props.completeTask(props.id, props.title); props.incrementCompleted((prev) => {return prev + 1;});}}>&#10003;</div>
+            <div className="timertodolistitem" id={`timertodolistitem${props.id}`} style={{"background": props.isActive(props.id)? "#5c02c9" : "#7b00e8", "color": props.isActive(props.id) ? "orange" : "white"}} onClick={(e) => {
+                // setInProgress(!inProgress);
+                props.updateActive(props.id, props.title);
+                // e.stopPropagation();
+            }}>
+                <h4 className="timertodotitle">{props.title}</h4>
+            </div>
         </div>
     );
 }
