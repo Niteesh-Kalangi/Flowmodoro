8ed2a8acffb05d3a638070899198a73c21a58e90
dhanushk03 <35636765+dhanushk03@users.noreply.github.com>
2023-08-13 22:41:58 -0400

    Update TimerTodoList.js

--- timerapp/src/Components/CountdownTimer/TimerTodoList.js
+++ timerapp/src/Components/CountdownTimer/TimerTodoList.js
@@ -4,42 +4,11 @@ import TimerTodoListItem from "./TimerTodoListItem.js";
 import "./TimerTodoList.css";
 
 const TimerTodoList = (props) => {
-    const [todoList, setTodoList] = useState(() => {
-        let list = localStorage.getItem("todoList");
-        return list ? JSON.parse(list) : [];
-    });
-
-    useEffect(() => {
-        localStorage.setItem('todoList', JSON.stringify(todoList));
-    }, [todoList]);
-
-    const completeTask = (id, e) => {
-        var completedTask = null;
-        todoList.forEach(item => {
-          if (item.id == id) {
-            completedTask = item;
-          }
-        });
-        deleteTask(id, e);
-    };
-    
-    const deleteTask = (id, e) => {
-        setTimeout(function() {
-            setTodoList(
-            todoList.filter((item) => {
-                return item.id != id;
-            })
-            );
-        }, 200);
-        e.stopPropagation();
-    };
-    
-    
     return (
         <div className="timertodolist">
             <h3 style={{"textAlign": "center"}}>Click on the tasks you're working on. &#10003; them off when completed.</h3>
             {/* <hr style={{height:"1px"}}></hr> */}
-            {todoList.map((item) => {
+            {props.todoList.map((item) => {
                 return (
                     <div>
                         <TimerTodoListItem
@@ -48,7 +17,7 @@ const TimerTodoList = (props) => {
                             id={item.id}
                             updateActive={props.updateActive}
                             isActive={props.isActive}
-                            completeTask={completeTask}
+                            completeTask={props.completeTask}
                             incrementCompleted={props.incrementCompleted}
                         />
                     </div>
